30f3ba83120547b80de6514f55edef2937f61c3a
dorotakora <dorota.kora@o2.pl>
2019-09-17 20:48:19 +0200

    carousel-last-changes

--- src/components/carousel/MainCarousel.jsx
+++ src/components/carousel/MainCarousel.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 
 import "./mainCarousel.css";
-
+import Carousel from "react-bootstrap/Carousel";
 import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
 
 import "font-awesome/css/font-awesome.min.css";
 import FontAwesome from "react-fontawesome";
-import Carousel from "react-bootstrap/Carousel";
+
+import cyclist from "../../assets/images/cyclist.png";
+import kettlebell from "../../assets/images/kettlebell.png";
+import speedway from "../../assets/images/speedway.png";
+import technologies from "../../assets/images/technologies.png";
 
 const styles = { height: "100vh", width: "100%", backgroundColor: "#f9f7f7" };
 
@@ -102,6 +106,7 @@ function MainCarousel() {
             </div>
 
             <div>
+              {window.innerWidth >= 768 ? icons : null}
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
@@ -112,18 +117,7 @@ function MainCarousel() {
                 sunt in culpa qui officia deserunt mollit anim id est laborum.
               </p>
 
-              <div className="carousel-center-icons">
-                <FontAwesome
-                  className="fab fa-linkedin"
-                  name="linkedin"
-                  size="2x"
-                />
-                <FontAwesome
-                  className="fab fa-github-square"
-                  name="github"
-                  size="2x"
-                />
-              </div>
+              {window.innerWidth < 768 ? icons : null}
             </div>
           </div>
         </div>
@@ -148,6 +142,7 @@ function MainCarousel() {
             </div>
 
             <div>
+              {window.innerWidth >= 768 ? icons : null}
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
@@ -157,68 +152,37 @@ function MainCarousel() {
                 nulla pariatur. Excepteur sint occaecat cupidatat non proident,
                 sunt in culpa qui officia deserunt mollit anim id est laborum.
               </p>
-
-              <div className="carousel-center-icons">
-                <FontAwesome
-                  className="fab fa-linkedin"
-                  name="linkedin"
-                  size="2x"
-                  style={{ color: "rgb(46, 204, 113)" }}
-                />
-                <FontAwesome
-                  className="fab fa-github-square"
-                  name="github"
-                  size="2x"
-                  style={{ color: "rgb(46, 204, 113)" }}
-                />
-              </div>
+              {window.innerWidth < 768 ? icons : null}
             </div>
           </div>
         </div>
       </Carousel.Item>
       <Carousel.Item>
         <div style={styles}>
-          <div className="carousel-center" style={{ width: "80%" }}>
+          <div className="carousel-center hobbies" style={{ width: "80%" }}>
             <div>
               <div className="green-line">
-                <h3>INTERESTS</h3>
+                <h3>HOBBIES & INTERESTS</h3>
                 <div></div>
               </div>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
             </div>
+            <div className="hobbies-list">
+              <div className="hobbies-list-item">
+                <img src={cyclist} alt="cyclist"></img>
+                <div>Outdoor cycling</div>
+              </div>
 
-            <div>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
-
-              <div className="carousel-center-icons">
-                <FontAwesome
-                  className="fab fa-linkedin"
-                  name="linkedin"
-                  size="2x"
-                  style={{ color: "rgb(46, 204, 113)" }}
-                />
-                <FontAwesome
-                  className="fab fa-github-square"
-                  name="github"
-                  size="2x"
-                  style={{ color: "rgb(46, 204, 113)" }}
-                />
+              <div className="hobbies-list-item">
+                <img src={speedway} alt="speedway"></img>
+                <div>Speedway fan</div>
+              </div>
+              <div className="hobbies-list-item">
+                <img src={kettlebell} alt="kettlebell"></img>
+                <div>Kettlebells</div>
+              </div>
+              <div className="hobbies-list-item">
+                <img src={technologies} alt="technologies"></img>
+                <div>New technologies</div>
               </div>
             </div>
           </div>
